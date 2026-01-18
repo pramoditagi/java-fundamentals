628cb4ffb49ca84030c3ebee58232abc9068701f
pramoditagi <itagipramod@gmail.com>
2026-01-18 14:51:31 +0530

    String, Datatype and Assignment operator programs

---
+++ CourseProblems/Datatypes.java
@@ -0,0 +1,46 @@
+package CourseProblems;
+
+public class Datatypes {
+  public static void main(String[] args) {
+
+    // byte
+    // 8 bits
+    byte b = 10;
+    System.out.println("byte: " + b);
+
+    // short
+    // 16 bits
+    short s = 1041;
+    System.out.println("short: " + s);
+
+    // int
+    // 32 bits
+    int i = 1012565;
+    System.out.println("int: " + i);
+
+    // long
+    // 64 bits
+    long l = 1012345;
+    System.out.println("long: " + l);
+
+    // float
+    // 32 bits
+    float f = 10.9f;
+    System.out.println("float: " + f);
+
+    // double
+    // 64 bits
+    double doub = 10.5;
+    System.out.println("double: " + doub);
+
+    // char
+    // 16 bits
+    char character = 'A';
+    System.out.println("char: " + character);
+
+    // boolean
+    boolean bool = true;
+    System.out.println("boolean: " + bool);
+    
+  }
+}
