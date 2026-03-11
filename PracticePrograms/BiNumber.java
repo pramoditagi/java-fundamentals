bf9bba1d12cb019ef54e379e3337ad10aadd587f
Satyapramod Itagi <itagipramod@gmail.com>
2026-03-12 01:16:15 +0530

    Conditional statement coding exercises and minor fixes (#10)
    Conditional statement coding exercises and minor fixes

--- PracticePrograms/BiNumber.java
+++ PracticePrograms/BiNumber.java
@@ -30,8 +30,49 @@ public class BiNumber {
     return number2;
   }
 
+  public int calculateLCM() {
+    if (number1 < 0 || number2 < 0)
+      return -1;
+
+    if (number1 == 0 || number2 == 0)
+      return 0;
+
+    int max = Math.max(number1, number2);
+    int lcm = max;
+
+    while (lcm % number1 != 0 || lcm % number2 != 0) {
+      lcm += max;
+    }
+    return lcm;
+  }
+
+  public int calculateGCD() {
+    if (number1 < 0 || number2 < 0)
+      return 1;
+
+    if (number1 == 0 || number2 == 0)
+      return 0;
+
+    if (number1 == number2)
+      return number1;
+
+    int smallest = Math.min(number1, number2);
+    int gcd = 0;
+
+    for (int i = smallest; i > 1; i--) {
+      if (number1 % i == 0 && number2 % i == 0) {
+        gcd = i;
+        break;
+      }
+    }
+    return gcd;
+  }
+
   public static void main(String[] args) {
-    BiNumber biNumber = new BiNumber(2, 3);
+    BiNumber biNumber = new BiNumber(48, 18);
+    System.out.println(biNumber.calculateLCM());
+    System.out.println(biNumber.calculateGCD());
+
     System.out.println(biNumber.add());
     System.out.println(biNumber.multiply());
 
@@ -40,5 +81,4 @@ public class BiNumber {
     System.out.println(biNumber.getNumber1());
     System.out.println(biNumber.getNumber2());
   }
-
 }
--- PracticePrograms/WhileNumberPlayer.java
+++ PracticePrograms/WhileNumberPlayer.java
@@ -31,5 +31,4 @@ public class WhileNumberPlayer {
     whileNumberPlayer.printSquaresUpToLimit();
     whileNumberPlayer.printCubesUpToLimit();
   }
-
 }
